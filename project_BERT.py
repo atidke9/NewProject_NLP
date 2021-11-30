b06e8b3ed8dce1aac22e606402fd91917a76f21c
atidke9 <80483562+atidke9@users.noreply.github.com>
2021-11-29 20:18:02 -0500

    Add files via upload

---
+++ project_BERT.py
@@ -0,0 +1,234 @@
+import transformers
+from transformers import BertModel, BertTokenizer, AdamW, get_linear_schedule_with_warmup
+import torch
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from collections import defaultdict
+from textwrap import wrap
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+
+# %matplotlib inline
+# %config InlineBackend.figure_format='retina'
+# sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+# HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#ADFF02", "#8F00FF"]
+# sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
+# rcParams['figure.figsize'] = 12, 8
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+train_data = pd.read_csv('train_cleaned.csv')
+val_data = pd.read_csv('val_cleaned.csv')
+test_data = pd.read_csv('test_cleaned.csv')
+
+class_names = [0,1]
+
+PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
+tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+
+import nltk
+token_lens = []
+for txt in train_data.cleaned_text:
+  tokens = nltk.word_tokenize(txt)
+  token_lens.append(len(tokens))
+
+fig, ax = plt.subplots(figsize=(10, 7))
+ax.hist(token_lens, bins=20)
+
+# Show plot
+plt.show()
+
+MAX_LEN = 160 #cannot be greater than 512
+
+class GPReviewDataset(Dataset):
+  def __init__(self, reviews, targets, tokenizer, max_len):
+    self.reviews = reviews
+    self.targets = targets
+    self.tokenizer = tokenizer
+    self.max_len = max_len
+  def __len__(self):
+    return len(self.reviews)
+  def __getitem__(self, item):
+    review = str(self.reviews[item])
+    target = self.targets[item]
+    encoding = self.tokenizer.encode_plus(
+      review,
+      add_special_tokens=True,
+      max_length=self.max_len,
+      return_token_type_ids=False,
+      pad_to_max_length=True,
+      return_attention_mask=True,
+      return_tensors='pt',
+    )
+    return {
+      'review_text': review,
+      'input_ids': encoding['input_ids'].flatten(),
+      'attention_mask': encoding['attention_mask'].flatten(),
+      'targets': torch.tensor(target, dtype=torch.long)
+    }
+
+def create_data_loader(df, tokenizer, max_len, batch_size):
+  ds = GPReviewDataset(
+    reviews=df.cleaned_text.to_numpy(),
+    targets=df.Label.to_numpy(),
+    tokenizer=tokenizer,
+    max_len=max_len
+  )
+  return DataLoader(
+    ds,
+    batch_size=batch_size,
+    num_workers=4
+  )
+
+BATCH_SIZE = 16
+train_data_loader = create_data_loader(train_data, tokenizer, MAX_LEN, BATCH_SIZE)
+val_data_loader = create_data_loader(val_data, tokenizer, MAX_LEN, BATCH_SIZE)
+test_data_loader = create_data_loader(test_data, tokenizer, MAX_LEN, BATCH_SIZE)
+
+"""Example text"""
+# bert_model = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
+#
+# sample_txt = 'When was I last outside? I am stuck at home for 2 weeks.'
+# encoding = tokenizer.encode_plus(
+#   sample_txt,
+#   max_length=32,
+#   add_special_tokens=True, # Add '[CLS]' and '[SEP]'
+#   return_token_type_ids=False,
+#   pad_to_max_length=True,
+#   return_attention_mask=True,
+#   return_tensors='pt',  # Return PyTorch tensors
+# )
+# encoding.keys()
+#
+# x = bert_model(
+#   input_ids=encoding['input_ids'],
+#   attention_mask=encoding['attention_mask']
+# )
+""""""
+
+
+class SentimentClassifier(nn.Module):
+  def __init__(self, n_classes):
+    super(SentimentClassifier, self).__init__()
+    self.bert = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
+    self.drop = nn.Dropout(p=0.3)
+    self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+    self.softmax = nn.Softmax(dim = 1)
+  def forward(self, input_ids, attention_mask):
+    bert_output = self.bert(
+      input_ids=input_ids,
+      attention_mask=attention_mask
+    )
+    output = self.drop(bert_output[1])
+    output = self.out(output)
+    return self.softmax(output)
+
+model = SentimentClassifier(len(class_names))
+model = model.to(device)
+
+# data = next(iter(train_data_loader))
+# print(data['input_ids'].shape)
+# print(data['attention_mask'].shape)
+# print(data['targets'].shape)
+#
+# input_ids = data['input_ids'].to(device)
+# attention_mask = data['attention_mask'].to(device)
+#
+# print(input_ids.shape)
+# print(attention_mask.shape)
+#
+# batch_out = model(input_ids, attention_mask)
+
+EPOCHS = 2
+optimizer = AdamW(model.parameters(), lr=2e-5, correct_bias=False)
+total_steps = len(train_data_loader) * EPOCHS
+scheduler = get_linear_schedule_with_warmup(
+  optimizer,
+  num_warmup_steps=0,
+  num_training_steps=total_steps
+)
+loss_fn = nn.CrossEntropyLoss().to(device)
+
+def train_epoch( model, data_loader, loss_fn,
+  optimizer, device, scheduler, n_examples
+):
+  model = model.train()
+  losses = []
+  correct_predictions = 0
+  for d in data_loader:
+    input_ids = d["input_ids"].to(device)
+    attention_mask = d["attention_mask"].to(device)
+    targets = d["targets"].to(device)
+    outputs = model(
+      input_ids=input_ids,
+      attention_mask=attention_mask
+    )
+    _, preds = torch.max(outputs, dim=1)
+    loss = loss_fn(outputs, targets)
+    correct_predictions += torch.sum(preds == targets)
+    losses.append(loss.item())
+    loss.backward()
+    nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+    optimizer.step()
+    scheduler.step()
+    optimizer.zero_grad()
+  return correct_predictions / n_examples, np.mean(losses)
+
+def eval_model(model, data_loader, loss_fn, device, n_examples):
+  model = model.eval()
+  losses = []
+  correct_predictions = 0
+  with torch.no_grad():
+    for d in data_loader:
+      input_ids = d["input_ids"].to(device)
+      attention_mask = d["attention_mask"].to(device)
+      targets = d["targets"].to(device)
+      outputs = model(
+        input_ids=input_ids,
+        attention_mask=attention_mask
+      )
+      _, preds = torch.max(outputs, dim=1)
+      loss = loss_fn(outputs, targets)
+      correct_predictions += torch.sum(preds == targets)
+      losses.append(loss.item())
+  return correct_predictions.double() / n_examples, np.mean(losses)
+
+history = defaultdict(list)
+best_accuracy = 0
+for epoch in range(EPOCHS):
+  print(f'Epoch {epoch + 1}/{EPOCHS}')
+  print('-' * 10)
+  train_acc, train_loss = train_epoch(
+    model,
+    train_data_loader,
+    loss_fn,
+    optimizer,
+    device,
+    scheduler,
+    len(train_data)
+  )
+  print(f'Train loss {train_loss} accuracy {train_acc}')
+  # val_acc, val_loss = eval_model(
+  #   model,
+  #   val_data_loader,
+  #   loss_fn,
+  #   device,
+  #   len(val_data)
+  # )
+  # print(f'Val   loss {val_loss} accuracy {val_acc}')
+  print()
+  history['train_acc'].append(train_acc)
+  history['train_loss'].append(train_loss)
+  # history['val_acc'].append(val_acc)
+  # history['val_loss'].append(val_loss)
+  # if val_acc > best_accuracy:
+  #   torch.save(model.state_dict(), 'best_model_state.bin')
+  #   best_accuracy = val_acc
